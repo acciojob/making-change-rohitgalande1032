c9aa6cc1f6a66c92e5d91e332a6ef3ae57031c42
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-17 15:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const makeChange = (c) => {
   // your name here
-};
+  const obj = {
+      "q" : 25,
+      "d": 10,
+      "n": 5,
+      "p": 1,
+  };
+  
+  let result = {
+    "q": 0,
+    "d": 0,
+    "n": 0,
+    "p": 0,
+  };
+  
+  
+  let keys = Object.keys(obj);
+  for(const key of keys) {
+    while(c>=obj[key]){
+      result[key]+=1;
+      c-=obj[key]
+    }
+  }
+  return result;
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
